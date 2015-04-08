0400a7bd76f28cbcb602e393d4808d0cba907e21
Zhifei Fang <facetothefate@gmail.com>
2015-04-08 16:06:25 -0400

    Create the basic staff

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  var sourceFiles=[
+    'Header.js',
+    'URLParser.js',
+    'Nav.js',
+    'HL.js',
+    'Footer.js',
+  ];
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat:{
+      options:{
+        banner:'/*! <%= pkg.name %> - v<%= pkg.version %> - Released: <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+
+      },
+      dist:{
+        src:sourceFiles,
+        dest:'/release/<%= pkg.name %>-<%= pkg.version %>.js',
+      }
+    },
+    jshint:{
+      beforeconcat:sourceFiles,
+      //afterconcat:'js/<%= pkg.name %>-<%= pkg.version %>.js',
+    },
+    uglify:{
+      options:{
+        banner:'/release/*! <%= pkg.name %> - v<%= pkg.version %> - Realesed: <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build:{
+        src:'<%= pkg.name %>-<%= pkg.version %>.js',
+        dest:'<%= pkg.name %>-<%= pkg.version %>.min.js'
+      },
+    },
+    watch:{
+      files:sourceFiles,
+      tasks:['default']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //task configuration
+  grunt.registerTask('default',['concat','jshint','uglify']);
+  //grunt.registerTask('compress', ['uglify']);
+
+};
---
+++ src/Header.js
@@ -0,0 +1,5 @@
+!function(){
+	"use strict";
+	var HL={version:"0.1.0"};
+
+}();
